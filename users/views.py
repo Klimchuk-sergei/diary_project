53ffa2dfaff17d40d375ad92a378354363e02f38
Klimchuk-sergei <klim.work@mail.ru>
2025-11-11 16:57:57 +0300

    create users views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm  # Форма для регистрации, и создания пользователя в БД
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')  # перенаправление на страницу входа после успешной регистрации
