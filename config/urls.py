de405a11fdafcdcee7212f3c704045482a3da6a1
Klimchuk-sergei <klim.work@mail.ru>
2025-11-11 18:30:22 +0300

    upd urls

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),  # авторизация, login, logout
+    path('users/', include('users.urls', namespace='users')),  # регистрация, профиль
+    path('', include('diary.urls', namespace='diary')),  # дневник, main page
 ]
