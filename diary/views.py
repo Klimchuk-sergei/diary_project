b837267e250231e4ab0264f7e4cbbd9795163960
Klimchuk-sergei <klim.work@mail.ru>
2025-11-12 14:13:46 +0300

    create dockerfile

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Entry
 
-# Create your views here.
+# Просмотр списка записей
+class EntryListView(LoginRequiredMixin, ListView): # Просмотр только для авторизованых пользователей
+    model = Entry
+    context_object_name = 'entries'
+    template_name = 'diary/entry_list.html'
+
+    # Получение списка записей для текущего пользователя
+    def get_queryset(self):
+        return Entry.objects.filter(user=self.request.user)
+
+# Просмотр отдельной записи
+class EntryDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView): # Просмотр только для авторизованых пользователей
+    model = Entry
+    context_object_name = 'entry'
+    template_name = 'diary/entry_detail.html'
+
+    # Проверка, что пользователь имеет доступ к этой записи
+    def test_func(self):
+        entry = self.get_object()
+        return entry.user == self.request.user
